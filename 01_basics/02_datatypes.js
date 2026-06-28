12ec2bdbeb010fc3917922d0766b3736935fd594
Arun6499 <patilarun6499@gmail.com>
2026-06-28 07:23:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser so it won't work 
+
+console.log(3+3) // code readability should be high
+console.log("arun")
+
+let name = "arun"
+let age = 26
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint  
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
+
+
+
+
+
