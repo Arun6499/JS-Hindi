467e2ae85014cc3ae0f4398d5510373bd7e6b67b
Arun6499 <patilarun6499@gmail.com>
2026-07-01 16:42:19 +0000

    Conversion Operation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score="33adf"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33ajh" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
