6bdf545970f180b8a9efaf0188b83833f544b4f1
Arun6499 <patilarun6499@gmail.com>
2026-07-05 05:25:01 +0000

    conversion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -23,5 +23,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let somenumber = 33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************** Operations *******************
+
+let value =3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " welcome";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//console.log((3+4)*5*3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
