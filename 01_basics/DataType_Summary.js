ed43b9d012904799d4498db6aae649332ebc2c97
Arun6499 <patilarun6499@gmail.com>
2026-07-05 06:05:22 +0000

    DataTypes

---
+++ 01_basics/DataType_Summary.js
@@ -0,0 +1,42 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol,
+// BigInt
+
+const score = 100
+const scroreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id);
+// console.log(anotherId);
+console.log(id === anotherId);
+
+const bigNumber = 343545456456566n 
+
+
+
+// Reference or Non-Primitive
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj = {
+    name:"Arun",
+    age:26
+}
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof outsideTemp); // object
+
+console.log(typeof isLoggedIn);  // boolean
+
+console.log(typeof myobj); // object
