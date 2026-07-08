aba3f4c29b7417f9d6faf7b4d8e6140a8bc5fb75
Arun6499 <patilarun6499@gmail.com>
2026-07-08 16:55:42 +0000

    functions

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    name: "arun",
+    age: 45,
+    welcomeMessage: function(){
+        console.log(`${this.name}, welcome to website`);
+        console.log(this);
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.name="ram"
+// user.welcomeMessage()
+
+//console.log(this);
+
+// function chai(){
+//     let username = "arun"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "arun"
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = "arun"
+    console.log(this);
+}
+
+//chai()
+
+// const addTwo =(num1,num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo =(num1,num2) => num1 + num2
+
+// const addTwo =(num1,num2) => (num1 + num2)
+
+const addTwo =(num1,num2) => ({username:"Arun"})
+
+console.log(addTwo(3,4));
+
+const myArray = [2,5,6,7,4]
