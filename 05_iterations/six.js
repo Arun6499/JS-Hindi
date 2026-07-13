b8cd082c6487f36b1c3e6fab453c1f6969c3d0de
Arun6499 <patilarun6499@gmail.com>
2026-07-13 17:52:58 +0000

    filters

---
+++ 05_iterations/six.js
@@ -0,0 +1,34 @@
+//const coding = ["js", "ruby", "java", "python"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+    
+// } )
+
+// console.log(values);
+
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNum = myNum.filter( (num) => num > 5 )
+
+// const newNum = myNum.filter( (num) => {
+//     return num > 5
+// } )
+
+// const newNums = []
+
+// myNum.forEach ( (num) => {
+//     if (num > 5) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+// const books = [
+//     {
+//         title:'Book one',genre:'fiction',publish:1976, edition:2004},
+//     }
+// ]
+
