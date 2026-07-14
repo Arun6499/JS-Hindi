11dd964eb3d26d2aa51f77fc303af2845af4f25b
Arun6499 <patilarun6499@gmail.com>
2026-07-14 00:53:34 +0000

    filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -26,9 +26,40 @@ const myNum = [1,2,3,4,5,6,7,8,9,10]
 
 // console.log(newNums);
 
-// const books = [
-//     {
-//         title:'Book one',genre:'fiction',publish:1976, edition:2004},
-//     }
-// ]
+const books = [
+    {
+        title: 'Book one', genre: 'fiction', publish: 1976, edition: 2004
+    },
+    {
+        title: 'Book two', genre: 'Non-fiction', publish: 1966, edition: 2000
+    },
+    {
+        title: 'Book three', genre: 'History', publish: 2001, edition: 2006
+    },
+    {
+        title: 'Book four', genre: 'Science', publish: 1999, edition: 2004
+    },
+    {
+        title: 'Book five', genre: 'fiction', publish: 1988, edition: 2010
+    },
+    {
+        title: 'Book six', genre: 'fiction', publish: 2011, edition: 2016
+    },
+    {
+        title: 'Book seven', genre: 'Non-fiction', publish: 1999, edition: 2004
+    },
+    {
+        title: 'Book eight', genre: 'History', publish: 2008, edition: 2014
+    },
+    {
+        title: 'Book nine', genre: 'Science', publish: 1999, edition: 2004
+    },
+    {
+        title: 'Book ten', genre: 'fiction', publish: 2009, edition: 2014
+    }
+];
 
+//const userBooks  = books.filter( (book) => book.publish > 2000 )
+
+const userBooks = books.filter((book) => book.genre === 'fiction' && book.publish > 2000);
+console.log(userBooks);
